389aad218ca6c2ad6ec62819b7ee1bbeb5af74de
Alexander Aksenov <aksenov@smartway.today>
2025-04-02 23:31:03 +0300

    fix vite

--- sw-code-FE/vite.config.ts
+++ sw-code-FE/vite.config.ts
@@ -9,7 +9,10 @@ export default defineConfig(({ mode }) => ({
     allowedHosts: "all",
     host: "0.0.0.0",
     port: 3000,
-    strictPort: true, 
+    strictPort: true,
+    hmr: {
+      host: "code-interview.smartway.today"
+    },
   },
   plugins: [
     react(),
