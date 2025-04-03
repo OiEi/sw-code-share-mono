7a88a25a4bf8edbd1df714e6c0736bd261e6ceb7
Alexander Aksenov <aksenov@smartway.today>
2025-04-03 16:22:05 +0300

    task edit (mmm hueta)

--- sw-code-BE/services/task/task.go
+++ sw-code-BE/services/task/task.go
@@ -3,6 +3,7 @@ package task
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,63 +16,84 @@ type Task struct {
 	Solution string `json:"solution"`
 }
 
-type Tasks map[string][]Task
+type Tasks map[string]map[string][]Task
 
 func GetTasks() Tasks {
-	tasks := make(map[string][]Task)
+	tasks := make(map[string]map[string][]Task)
 
 	tasksPath := os.Getenv("TASKS_PATH")
 	if tasksPath == "" {
 		tasksPath = "./tasks" // путь по умолчанию в контейнере
 	}
 
-	dirs, err := os.ReadDir(tasksPath)
+	langDirs, err := os.ReadDir(tasksPath)
 	if err != nil {
 		fmt.Printf("Error reading tasks directory: %v\n", err)
 		return tasks
 	}
 
-	for _, dir := range dirs {
-		if !dir.IsDir() {
+	//папки языков
+	for _, langDir := range langDirs {
+		if !langDir.IsDir() {
 			continue
 		}
 
-		dirName := dir.Name()
-		dirPath := filepath.Join(tasksPath, dirName)
+		langDirName := langDir.Name()
+		langDirPath := filepath.Join(tasksPath, langDirName)
 
-		//read from subd
-		files, err := os.ReadDir(dirPath)
+		langTasksDirs, err := os.ReadDir(langDirPath)
 		if err != nil {
-			fmt.Printf("Error reading directory %s: %v\n", dirPath, err)
-			continue
+			fmt.Printf("Error reading tasks directory: %v\n", err)
+			return tasks
 		}
 
-		for _, file := range files {
-			if file.IsDir() || !strings.HasSuffix(file.Name(), ".json") {
+		for _, langTasksDir := range langTasksDirs {
+			if !langDir.IsDir() {
 				continue
 			}
 
-			//get "color" for task
-			fileName := file.Name()
-			color := strings.Split(fileName, "_")[0]
-
-			filePath := filepath.Join(dirPath, fileName)
-
-			data, err := os.ReadFile(filePath)
+			langTasksDirName := langTasksDir.Name()
+			langTaskDirPath := filepath.Join(langDirPath, langTasksDirName)
+			files, err := os.ReadDir(langTaskDirPath)
 			if err != nil {
-				fmt.Printf("Error reading file %s: %v\n", filePath, err)
+				fmt.Printf("Error reading directory %s: %v\n", langDirPath, err)
 				continue
 			}
 
-			var task Task
-			if err := json.Unmarshal(data, &task); err != nil {
-				fmt.Printf("Error parsing JSON from file %s: %v\n", filePath, err)
-				continue
-			}
+			for _, file := range files {
+				if file.IsDir() || !strings.HasSuffix(file.Name(), ".json") {
+					continue
+				}
 
-			task.Color = color
-			tasks[dir.Name()] = append(tasks[dir.Name()], task)
+				//get "color" for task
+				fileName := file.Name()
+				color := strings.Split(fileName, "_")[0]
+
+				filePath := filepath.Join(langTaskDirPath, fileName)
+
+				data, err := os.ReadFile(filePath)
+				if err != nil {
+					fmt.Printf("Error reading file %s: %v\n", filePath, err)
+					continue
+				}
+
+				var task Task
+				if err := json.Unmarshal(data, &task); err != nil {
+					fmt.Printf("Error parsing JSON from file %s: %v\n", filePath, err)
+					continue
+				}
+
+				task.Color = color
+
+				if tasks[langDirName] == nil {
+					tasks[langDirName] = make(map[string][]Task)
+				}
+
+				tasks[langDirName][langTasksDirName] = append(tasks[langDirName][langTasksDirName], task)
+				log.Println(tasks)
+			}
 		}
+
 	}
 
 	return tasks
