137636d6eaabb51c4fab9d4edfe7ed2e753ccaa9
cutemiya <k-shaipov@mail.ru>
2025-04-10 21:10:16 +0300

    fx

--- sw-code-FE/src/lib/constant/api.routes.ts
+++ sw-code-FE/src/lib/constant/api.routes.ts
@@ -2,7 +2,7 @@ const apiUrl = import.meta.env.VITE_API_HOST;
 const wsUrl = import.meta.env.VITE_WEBSOCKET_HOST;
 
 export const ROUTES = {
-  WS: (roomId?: string) => roomId ? `/ws?roomId=${roomId}` : '/auth/ws',
+  WS: (roomId?: string) => roomId ? `/api/ws?roomId=${roomId}` : '/auth/ws',
   GET_TASKS: '/auth/task',
   AUTH: '/auth'
 };
